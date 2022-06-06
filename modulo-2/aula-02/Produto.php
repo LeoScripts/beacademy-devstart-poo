2b44fb86d33e9344dff45cf6733ce8cdb53c1ac3
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-06-06 17:00:07 -0300

    feat: encapsulation part 2

--- modulo-2/aula-02/Produto.php
+++ modulo-2/aula-02/Produto.php
@@ -4,6 +4,33 @@
 declare(strict_types=1);
 
 class Produto {
+  // private = atributo privado
   private string $nome; 
   private float $valor;
+
+  // metodos --------------------------------------
+
+  // acessando o atributo privdo nome
+  public function pegarNome(): string
+  {
+    return $this->nome;
+  }
+
+  // acessando o atributo privado nome e alterando o seu valor
+  public function alterarNome(string $novoNome): void
+  {
+     $this->nome = $novoNome;
+  }
+
+  // acessando o atributo privado valor e alterando o seu valor se 
+  public function alterarValor(float $novoValor): void
+  {
+    // aqui estamos corrigindo a questao do valor
+    // ou seja nao aceitamos numeros negativos 
+    // porque isso e um preco
+    if($novoValor < 0){
+      die('Ops, valor nao pode ser negativo');
+    }
+     $this->valor = $novoValor;
+  }
 }
--- modulo-2/aula-02/index.php
+++ modulo-2/aula-02/index.php
@@ -3,12 +3,19 @@
 include 'Produto.php';
 
 $p1 = new Produto();
-$p1->nome = 'Tenis para corrida';
-$p1->valor = 299;
+$p1->alterarNome('Tenis para corrida');
+$p1->alterarValor(299);
 
-$p2 = new Produto();
-$p2->nome = 'Calça jeans';
-$p2->valor = -100;
+
+// ex 1 : atributo bublicos  -----------------------------
+
+// $p1 = new Produto();
+// $p1->nome = 'Tenis para corrida';
+// $p1->valor = 299;
+
+// $p2 = new Produto();
+// $p2->nome = 'Calça jeans';
+// $p2->valor = -100;
 
 var_dump($p1);
-var_dump($p2);
+// var_dump($p2);
