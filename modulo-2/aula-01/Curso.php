702e80e20dde5324327cd8ef80bfbabd3b4ee41c
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-06-06 14:49:50 -0300

    feat: exercice of abstraction

---
+++ modulo-2/aula-01/Curso.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+class Curso {
+  public $id;
+  public $nome; 
+  public $duracao;
+  public $inicio;
+  public $fim;
+  public $professor;
+}
--- modulo-2/aula-01/Professor.php
+++ modulo-2/aula-01/Professor.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-class Aluno {
+class Professor {
   public $matricula;
   public $nome; 
   public $cpf;
   public $email;
+  public $curso;
 }
