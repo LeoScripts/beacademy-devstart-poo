b0e1bd2785c34fc6e362676cbab2dc45777bcc39
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-06-10 20:00:36 -0300

    feat: first URLs example

--- modulo-2/projeto-final/public/index.php
+++ modulo-2/projeto-final/public/index.php
@@ -11,16 +11,32 @@ use App\Controller\ErrorController;
 
 $url = explode('?', $_SERVER['REQUEST_URI'])[0];
 
-if($url === '/'){
-  $c = new IndexController();
-  $c->indexAction();
-} elseif ($url === '/login') {
-  $c = new IndexController();
-  $c->loginAction();
-} elseif ($url === '/produtos'){
-  $p = new ProductController();
-  $p->listAction();
-} else {
-  $p = new ErrorController();
-  $p->notFoundAction();
-}
+
+// este este e um abstração dos if logo abaixo comentados
+// clean code né man kkkk
+$routes = [
+  '/' => [
+    //controller estou selecionando 
+    'controller' => IndexController::class,
+    // method - qual metodo do controller usar
+    'method' => 'indexAction' 
+  ],
+  '/produtos' => [
+    'controller' => ProductController::class,  // apos a => e o mesmo que - App\Controller\ProductController
+    'method' => 'listAction',
+  ]
+  ];
+
+// if($url === '/'){
+//   $c = new IndexController();
+//   $c->indexAction();
+// } elseif ($url === '/login') {
+//   $c = new IndexController();
+//   $c->loginAction();
+// } elseif ($url === '/produtos'){
+//   $p = new ProductController();
+//   $p->listAction();
+// } else {
+//   $p = new ErrorController();
+//   $p->notFoundAction();
+// }
